62cacb132761625b2b9a8ff17a68d7de641534ad
yaostyle <x@tofr.me>
2022-03-27 19:55:33 -0700

    Get ProductSearch suggestions

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -40,5 +40,12 @@ namespace BlazorEcommerce.Server.Controllers
             var result = await _productService.SearchProducts(searchText);
             return Ok(result);
         }
+
+        [HttpGet("searchsuggestions/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _productService.GetProductSearchSuggestions(searchText);
+            return Ok(result);
+        }
     }
 }
